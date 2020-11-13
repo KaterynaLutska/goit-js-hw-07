37b15639c9e0f7f1351ee04c8ccc502b163b8641
KaterynaLutska <ket.lutska@gmail.com>
2020-11-13 19:36:44 +0200

    add hw task 1-3

---
+++ task-01.js
@@ -0,0 +1,12 @@
+// task 01-01 //
+
+// const numberOfLi = document.querySelectorAll('li.item').length;
+// console.log(`В списку ${numberOfLi} категорії.`);
+
+// // task 01-02 // 
+
+// const nameOfCategories = document.querySelectorAll('h2').forEach((name) => {
+//     console.log(`Категорія: ${name.textContent}\nКількість елементів: ${name.nextElementSibling.childElementCount}`)  
+// })
+
+    //innerHTML
---
+++ task-02.js
@@ -0,0 +1,26 @@
+// task 02 //
+
+// const ingredients = [
+//   'Картошка',
+//   'Грибы',
+//   'Чеснок',
+//   'Помидоры',
+//   'Зелень',
+//   'Приправы',
+// ];
+
+// const ingredientsList = document.createElement('ul')
+
+// const ingredientItem = ingredients.forEach((el) =>
+//     (el = document.createElement('li'),
+//     ingredientsList.append(el)
+//   ))
+
+//  console.log(ingredientItem);
+
+/* 
+Напиши скрипт, который для каждого элемента массива
+ingredients создаст отдельный li, 
+после чего вставит все li за одну операцию в список
+ul.ingredients. 
+Для создания DOM-узлов используй document.createElement() */
